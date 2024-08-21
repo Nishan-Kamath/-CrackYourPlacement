4b56fa294e55d8f6322b742ea2c371f618ac4198
Nishan Kamath <nishankamath@gmail.com>
2024-08-21 21:07:48 +0530

    day11

---
+++ Mathematical Problems/7. Reverse Integer/code.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long digit = 0;
+        while(x != 0){
+            digit = (digit*10) + (x%10);
+            x /= 10;
+        }
+        if(digit > INT_MAX || digit < INT_MIN)
+            return 0;
+        return int(digit);
+    }
+};
